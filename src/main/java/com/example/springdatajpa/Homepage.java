d4abc1417bbf1adf9f4cd9b571f3b4d32398878f
Abhinov Gogoi <abhinov-gogoi@users.noreply.github.com>
2021-03-25 13:26:10 +0530

    homepage current date andtime

--- src/main/java/com/example/springdatajpa/Homepage.java
+++ src/main/java/com/example/springdatajpa/Homepage.java
@@ -3,10 +3,21 @@ package com.example.springdatajpa;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class Homepage {
+
     @RequestMapping("/")
     public String sayHello() {
-        return "Welcome to Spring Data JPA";
+
+        return "Welcome to Spring Data JPA. Your Current date is : "+ calcTime();
+    }
+
+    public String calcTime() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'and time is : 'HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
     }
 }
